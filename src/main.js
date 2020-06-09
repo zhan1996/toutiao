d1cf35c3229029f435dab31d80f60736739e2e02
Mr.Zhan <2695789929@qq.com>
2020-06-09 12:39:44 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 
+import router from '@/router'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container-login">
+      登录
+    </div>
+</template>
+           
+<script>
+export default{
+      name:'login-page'       
+}
+</script>
+<style scoped>
+            
+</style>
