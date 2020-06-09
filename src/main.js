231c3bb754ef0f94748dc20956f58159bbcd5949
Mr.Zhan <2695789929@qq.com>
2020-06-09 20:07:01 +0800

    登录验证完成

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import App from './App.vue'
 
 import router from '@/router'
 
+import axios from 'axios'
+// 基准路径
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 全局挂载
+Vue.prototype.$http = axios
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
     <div class="container-login">
       <el-card>
           <img class="logo" src="../../assets/logo_index.png" alt="">
-          <el-form>
-              <el-form-item>
+          <el-form ref="loginForm" :model="loginForm" :rules="loginRules" >
+              <el-form-item prop="mobile">
                   <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input style="width:240px; margin-right:8px" palceholder=""></el-input>
+              <el-form-item prop="code">
+                  <el-input style="width:240px; margin-right:8px" v-model="loginForm.code" placeholder="请输入验证码"></el-input>
                 <el-button>发送验证码</el-button>
               </el-form-item>
               <el-form-item>
                   <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
               </el-form-item>
               <el-form-item>
-                  <el-button type="primary" style="width:100%">登 录</el-button>
+                  <el-button @click="login()" type="primary" style="width:100%">登 录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -25,11 +25,42 @@
 export default{
       name:'login-page',
       data(){
+        //   声明自定义校验函数
+        const checkMobile = (rule,value,callback) => {
+            if(!/^1[3-9]\d{9}$/.test(value)) return callback(new Error('手机号不正确'))
+            callback()
+        }
           return {
               loginForm:{
                   mobile:'',
                   code:''
-              }
+              },
+            //   校验规则对象
+            loginRules:{
+                mobile:[
+                {required:true,message:'请输入手机号',trigger:'blur'},
+                // 自定义校验
+                {validator:checkMobile,trigger:'blur'}
+                ],
+            code:[
+                {required:true,message:'请输入密码',trigger:'blur'},
+                {len:6,message:'请输入6个字符',trigger:'blur'}
+                ]
+            }
+            
+          }
+      },
+      methods:{
+          login(){
+              this.$refs.loginForm.validate((valid)=>{
+                  if(valid){
+                      this.$http.post('/authorizations',this.loginForm).then(()=>{
+                          this.$router.push('/')
+                      }).catch(()=>{
+                          this.$message.error('手机号或验证码错误')
+                      })
+                  }
+              })
           }
       }       
 }
